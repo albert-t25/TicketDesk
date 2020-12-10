dadfe40a2a0cadc41fe1c67bf4e05f43a744423f
Albert T <at@pragmatic.al>
2020-12-10 13:18:23 +0100

    fixed log

--- TicketDesk/TicketDesk.Web.Identity/Model/SmsHelper.cs
+++ TicketDesk/TicketDesk.Web.Identity/Model/SmsHelper.cs
@@ -82,7 +82,7 @@ namespace TicketDesk.Web.Identity.Model
                     if (response.Contains("Success") && message.Contains("Authentication accepted"))
                     {
                         Console.WriteLine("Authenticated");
-                        Log.Info("Authenticated");
+                        log.Info("Authenticated");
                         stream.Flush();
                         connected = true;
                         return tcpClient;
@@ -90,7 +90,7 @@ namespace TicketDesk.Web.Identity.Model
                     else
                     {
                         Console.WriteLine("Credentials error! Can't Authenticate");
-                        Log.Error("Credentials error! Can't Authenticate");
+                        log.Error("Credentials error! Can't Authenticate");
                         tcpClient.Close();
                         connected = false;
                         return tcpClient;
@@ -103,7 +103,7 @@ namespace TicketDesk.Web.Identity.Model
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
-                Log.Error("Error opening connection with SMS Server", ex);
+                log.Error("Error opening connection with SMS Server", ex);
             }
 
             connected = false;
@@ -123,7 +123,7 @@ namespace TicketDesk.Web.Identity.Model
             }
             catch (Exception ex)
             {
-                Log.Error("Error closing connection", ex);
+                log.Error("Error closing connection", ex);
             }
         }
 
@@ -148,18 +148,18 @@ namespace TicketDesk.Web.Identity.Model
                 byte[] smsResp = new byte[1000];
                 stm.Read(smsResp, 0, 1000);
                 response = asen.GetString(smsResp);
-                Log.Error("Response " + response);
+                log.Error("Response " + response);
                 if (!String.IsNullOrEmpty(response))
                 {
                     stm.Read(smsResp, 0, 1000);
                     message = asen.GetString(smsResp);
-                    Log.Info("smsResp " + message);
+                    log.Info("smsResp " + message);
                     if (!String.IsNullOrEmpty(message))
                     {
                         stm.Read(smsResp, 0, 1000);
 
                         eventMsg = asen.GetString(smsResp);
-                        Log.Info("eventMsg " + eventMsg);
+                        log.Info("eventMsg " + eventMsg);
                         if (!String.IsNullOrEmpty(eventMsg))
                         {
                             String[] list = eventMsg.Split('\n');
@@ -180,7 +180,7 @@ namespace TicketDesk.Web.Identity.Model
             }
             catch (Exception ex)
             {
-                Log.Error("Error on sending SMS: ", ex);
+                log.Error("Error on sending SMS: ", ex);
             }
         }
 
